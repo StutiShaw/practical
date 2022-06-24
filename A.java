7e54fa21436833ce126204e257d861de2edbec53
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-24 13:02:04 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,9 @@
+class A{
+   public static void main (String[]args){
+    System.out.println("Name:Stuti Shaw");
+    System.out.println("Course:Java");
+    System.out.println("Stream:BCA");
+}
+
+
+}
