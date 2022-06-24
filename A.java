c8d8308058bbd5df584522cb337c41c63489eacd
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-24 13:03:05 +0530

    Update A.java

--- A.java
+++ A.java
@@ -1,5 +1,6 @@
 class A{
    public static void main (String[]args){
+      //to print statements
     System.out.println("Name:Stuti Shaw");
     System.out.println("Course:Java");
     System.out.println("Stream:BCA");
