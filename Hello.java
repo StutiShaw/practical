3c3533162a5088623682261a09964db518a00128
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-24 13:06:58 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello{
    public static void main (String[]args){
+      //to print the statements
     System.out.println("Welcome to coding world");
     System.out.println("Hello world");
 }
