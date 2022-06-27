99cff02d2c0be9944c29937ea54ec1f347133629
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-27 12:43:42 +0530

    Update Simpleif.java

--- Simpleif.java
+++ Simpleif.java
@@ -1,9 +1,9 @@
 class Simpleif{
 public static void main(String args[]){
-int x =5;
-int y =3;
+int x =5;//declare x=5
+int y =3;//declare y=3
 if(x+y < 10) {
-System.out.println("x + y is less than 10");
+System.out.println("x + y is less than 10");//to print the statement
 }
 }
 }
