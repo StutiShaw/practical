58d40f1d3feed738cb8cfb0a6b59f345c123647d
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-27 12:16:20 +0530

    Update Forloop.java

--- Forloop.java
+++ Forloop.java
@@ -1,9 +1,9 @@
 class Forloop{
 public static void main(String[] args){
 
-int i = 0;
-for (int j=1;j<=10;j++){
-i=i+j;
+int i = 0;// declare i=0
+for (int j=1;j<=10;j++){ // no. of time execution
+i=i+j;//logic
 }
 System.out.println("The sum of first 10 natural numbers:");
 }
