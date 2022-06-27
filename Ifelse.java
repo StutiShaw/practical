dda6f8cba6a0fc26cd887a1bc13421d4506f8b4e
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-27 12:51:20 +0530

    Update Ifelse.java

--- Ifelse.java
+++ Ifelse.java
@@ -1,11 +1,11 @@
 class Ifelse {
 public static void main(String[] args) {
-int a = 5;
+int a = 5;//Declaration of variable
 int b = 3;
 if(a+b < 10) {
-System.outprintln("a + b is less than 10");
+System.outprintln("a + b is less than 10");//to print a statement
 }
  else{
-System.outprintln("a + b is greater than 10");
+System.outprintln("a + b is greater than 10");//to print a statement
 }
 }
