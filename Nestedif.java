448051a4c52eb96b35c18e443a927637d1010430
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-27 13:09:24 +0530

    Update Nestedif.java

--- Nestedif.java
+++ Nestedif.java
@@ -1,21 +1,21 @@
 class Nestedif {
 public static void main(String[] args) {
-String address = "Kolkata, India";
-if(address.endswith("India")) {
+String address = "Kolkata, India";//declare string
+if(address.endswith("India")) {  //if statement
 if(address.contains("Dunlop")) {
-System.out.println("Your city is Dunlop");
+System.out.println("Your city is Dunlop");//to print a statement
 }
-else if(address.contains("Newtown")) {
+else if(address.contains("Newtown")) { // else if statement
 
-System.out.println("Your city is Newtown");
+System.out.println("Your city is Newtown");//to print a statement
 }
 else {
 
-System.out.printl(address.split(",")[0]);
+System.out.printl(address.split(",")[0]);//to print a statement
 }
 }
 else {
-System.out.println("You are not living in India"); 
+System.out.println("You are not living in India"); //to print a statement
 }
 }
 }
