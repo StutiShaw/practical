90049f3c060657aadf6a472d317b8f4ac9a15f83
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-27 12:24:58 +0530

    Update Dowhile.java

--- Dowhile.java
+++ Dowhile.java
@@ -1,11 +1,11 @@
 class Dowhile{
 public static void main(String[] args){
 
-int i = 0;
-System.out.println("First 10 even numbers \n");
+int i = 0;//declare i=0
+System.out.println("First 10 even numbers \n");// to print the statement
 do{
 System.out.println(i);
-i=i+2;
-}while(i<=10);
+i=i+2;//logic
+}while(i<=10);//no. of time execution
 }
 }
