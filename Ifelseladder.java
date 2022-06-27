b05fd4f2c355173d05948ba7b26056b12ae6247b
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-27 13:02:54 +0530

    Update Ifelseladder.java

--- Ifelseladder.java
+++ Ifelseladder.java
@@ -1,12 +1,12 @@
  public class Ifelseladder {
 public static void main(String[] args) {
-String name ="Riya";
+String name ="Riya";//declaration of string
 if(name =="Meena"){
-System.out.println("name is Meena");
+System.out.println("name is Meena");//to print a statement
 }else if(name =="Neha"){
-System.out.println("name is Neha");
+System.out.println("name is Neha");//to print a statement
 }else if(name =="Amit"){
-System.out.println("name is amit");
+System.out.println("name is amit");//to print a statement
 }else{
 System.out.println(name);
 }
