385dc80e67958de6a13aeb48936068b933493cce
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-27 13:23:05 +0530

    Update Evenodd.java

--- Evenodd.java
+++ Evenodd.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class EvenOdd {
 
     public static void main(String[] args) {
-
+// create a scanner 
         Scanner reader = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
+        //read integer from user
         int num = reader.nextInt();
-
+// check if the number is even or odd
         if(num % 2 == 0)
-            System.out.println(num + " is even");
+            System.out.println(num + " is even");// to print the statement
         else
-            System.out.println(num + " is odd");
+            System.out.println(num + " is odd");// to print the statement
     }
 }
