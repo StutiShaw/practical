99e2ce72b01d1f2c712b7a3f22afb119324b1df3
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-27 12:30:24 +0530

    Update Whileloop.java

--- Whileloop.java
+++ Whileloop.java
@@ -1,11 +1,11 @@
 class Whileloop {
 public static void main(String[] args) {
  
-int i = 0; 
-System.out.println("First 10 even numbers \n");
-while(i<=10) {
-System.out.println(i);
-i=i+2;
+int i = 0; //declare i=0
+System.out.println("First 10 even numbers \n");//to print the statement
+while(i<=10) {//no. of time execution
+System.out.println(i);//
+i=i+2;//logic
 } 
 }
 }
