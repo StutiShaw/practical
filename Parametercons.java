526c9f01796a849b508c7e460f51dc8c8b4d7b40
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-30 12:25:21 +0530

    Update Parametercons.java

--- Parametercons.java
+++ Parametercons.java
@@ -1,14 +1,14 @@
-public class Parametercons {
-	 String name;
+public class Parametercons {  // creating a class
+	 String name;  // declaring string
      Parametercons (String name){
-         this.name = name;
+         this.name = name;   
      System.out.println("cons called");
 
      }
      public static void main(String [] args) {
-         Parametercons obj=new Parametercons ("java");
-         Parametercons obj1=new Parametercons ("c++");
-         Parametercons obj2=new Parametercons ("c");
+         Parametercons obj=new Parametercons ("java");  // creating object
+         Parametercons obj1=new Parametercons ("c++");  // creating object
+         Parametercons obj2=new Parametercons ("c");    // creating object
      System.out.println("The name is" +  obj.name);
      System.out.println("The name is" +  obj1.name);
      System.out.println("The name is" +  obj2.name);
