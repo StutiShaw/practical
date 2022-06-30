e7d10ec3f23998e97f9310d005a309f328b7a439
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-06-30 12:20:41 +0530

    Update Defaultcons.java

--- Defaultcons.java
+++ Defaultcons.java
@@ -1,12 +1,12 @@
 public class DefaultCons {
 	private String name;
-	DefaultCons()
+	DefaultCons() //creating a constructor
 	{
 		name = "Stuti";
 		System.out.println("Cons Called");
 	}
 	public static void main(String args[]) {
-	DefaultCons obj=new DefaultCons();
+	DefaultCons obj=new DefaultCons();  // creating object
 	System.out.println("The name is "+obj.name);
 		
 	}
