a75a48c347bab58e5cdc740d611158267243168b
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-07-13 11:13:19 +0530

    Add files via upload

---
+++ Student_Encapsulate.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Student_Encapsulate {
+    private String name; // private data member
+    public String getName() { // getter method for name
+    	return name;
+}
+    public void setName(String name) { // setter method for name
+    this.name=name;
+}
+}
---
+++ Test_encapsulate.java
@@ -0,0 +1,9 @@
+package com;
+
+public class Test_encapsulate {
+	public static void main(String args[]) {
+		Student_Encapsulate obj = new Student_Encapsulate();
+		obj.setName("Stuti");// setting the value
+		System.out.println(obj.getName());// getting the value
+	}
+}
