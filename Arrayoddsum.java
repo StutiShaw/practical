8271b6c395986ee0e7f3663cbfcdbad68ade72fb
StutiShaw <107671149+StutiShaw@users.noreply.github.com>
2022-07-20 10:27:42 +0530

    Add files via upload

---
+++ Arrayoddsum.java
@@ -0,0 +1,31 @@
+package array;
+import java.util.Scanner;
+public class Arrayoddsum {
+
+     private static Scanner sc;
+	public static void main(String[] args) 
+		{
+			int Size, i, OddSum = 0;
+			sc = new Scanner(System.in);
+		 
+			System.out.print(" Please Enter Number of elements in an array : ");
+			Size = sc.nextInt();	
+			
+			int [] a = new int[Size];
+			
+			System.out.print(" Please Enter " + Size + " elements of an Array  : ");
+			for (i = 0; i < Size; i++)
+			{
+				a[i] = sc.nextInt();
+			}   
+
+			for(i = 0; i < Size; i++)
+			{
+				if(a[i] % 2 != 0)
+				{
+					OddSum = OddSum + a[i]; 
+				}
+			}		
+			System.out.println("\n The Sum of Odd Numbers in this Array = " + OddSum);
+		}
+	}
